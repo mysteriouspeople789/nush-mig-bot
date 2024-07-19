9817a4d7e3bc6c7db5558fd852151fd7ed8c0c9e
mysteriouspeople789 <dingchenghao283@gmail.com>
2024-07-19 18:02:04 +0800

    changed math24 to 1 minute game

--- main.py
+++ main.py
@@ -75,14 +75,14 @@ def restricted(func):
 # Handlers
 @restricted
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Welcome to MIG Bot! To begin, enter your full name:")
+    await update.message.reply_text("Welcome to MIG Bot! For verification, please enter your full name:")
     return NAME
 
 
 async def name(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.message.from_user
     context.user_data['name'] = update.message.text
-    await update.message.reply_text("Enter your class (e.g. 101):")
+    await update.message.reply_text("Please enter your class as well (e.g. 101):")
     return CLASS
 
 
@@ -92,7 +92,7 @@ async def clas(update: Update, context: ContextTypes.DEFAULT_TYPE):
         'user_id': user.id,
         'name': context.user_data['name'],
         'class': update.message.text,
-        'score': 0.0,
+        'points': 0.0,
     }
     users_collection.insert_one(user_data)
     await update.message.reply_text("Your data has been saved.")
@@ -102,7 +102,6 @@ async def clas(update: Update, context: ContextTypes.DEFAULT_TYPE):
 @restricted
 async def answer(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.message.from_user
-    print("/answer chat id", update.effective_chat.id)
     user_data = users_collection.find_one({'user_id': user.id})
     if user_data is None:
         await update.message.reply_text(
@@ -110,7 +109,7 @@ async def answer(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
 
     if context.args:
-        number = context.args[0]
+        number = context.args[0].strip()
         current_problem = problems_collection.find_one({'_id': 'current_problem'})
         problem_number = current_problem['number']
         if f"answer{problem_number}" in user_data:
@@ -166,10 +165,10 @@ async def notify_users():
     for user in users:
         user_id = user['user_id']
         user_attempts = user.get(f'answer{problem_number}')
-        prev_score = user['score']
+        prev_score = user['points']
         total_score = sum(10 if attempt == correct_answer else 0 for attempt in user_attempts)
         average_score = total_score / len(user_attempts)
-        users_collection.update_one({'user_id': user_id}, {'$inc': {'score': average_score}})
+        users_collection.update_one({'user_id': user_id}, {'$inc': {'points': average_score}})
         message = f"Your average score for problem {problem_number} is {round(average_score, 2)}, across all {len(user_attempts)}\
         submissions. Your total score is {round(prev_score + average_score, 2)}."
         await bot.send_message(chat_id=user_id, text=message)
@@ -241,19 +240,20 @@ def find_solution(nums):
 @restricted
 async def math24_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.message.from_user
+    user_data = users_collection.find_one({'user_id': user.id})
+    if user_data is None:
+        await update.message.reply_text(
+            "Please use the /start command to enter your name and class before using this command.")
+        return
     await cancel(update, context)
-    number1 = random.randrange(1, 10)
-    number2 = random.randrange(1, 10)
-    number3 = random.randrange(1, 10)
-    number4 = random.randrange(1, 10)
     context.user_data['game1_active'] = True
-    context.user_data['math24_numbers'] = [number1, number2, number3, number4]
-    context.user_data['game1_end_job'] = context.job_queue.run_once(game1_end_job, when=30,
+    context.user_data['correct_count'] = 0
+    context.user_data['game1_end_job'] = context.job_queue.run_once(game1_end_job, when=60,
                                                                     data=(update.message.chat_id, user.id, context))
-    await update.message.reply_text(f"Use the following numbers and the four operations (+, -, *, /) with brackets to "
+    await update.message.reply_text(f"Use the following 4 numbers and the 4 operations (+, -, *, /) with brackets to "
                                     f"achieve the number 24! You may use the numbers in any order. If you think there "
-                                    f"is no solution, answer -1.\nTime limit: 30 seconds."
-                                    f"\n\n{number1} {number2} {number3} {number4}")
+                                    f"is no solution, answer -1.\nAnswer as many as you can in 1 minute!")
+    await send_next_number(update, context)
 
 
 def is_valid_user_expression(user_expr, nums):
@@ -274,39 +274,47 @@ def is_valid_user_expression(user_expr, nums):
         return False
 
 
+async def send_next_number(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if 'correct_count' not in context.user_data or not context.user_data.get('game1_active', False):
+        return
+
+    # Generate two random numbers and their sum
+    number1 = random.randrange(1, 10)
+    number2 = random.randrange(1, 10)
+    number3 = random.randrange(1, 10)
+    number4 = random.randrange(1, 10)
+    context.user_data['math24_numbers'] = [number1, number2, number3, number4]
+
+    await update.message.reply_text(f"{number1} {number2} {number3} {number4}")
+
+
 async def math24_answer(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    job = context.user_data.get('game1_end_job')
-    if job:
-        job.schedule_removal()
-    user = update.message.from_user
     user_answer = update.message.text.strip()
-    correct = False
     solution = find_solution(context.user_data['math24_numbers'])
     if is_valid_user_expression(user_answer, context.user_data['math24_numbers']):
-        correct = True
+        context.user_data['correct_count'] += 1
         await update.message.reply_text("Correct!")
     elif user_answer == "-1" and solution == "No Solution":
-        correct = True
+        context.user_data['correct_count'] += 1
         await update.message.reply_text("Correct!")
     elif solution == "No Solution":
         await update.message.reply_text("Wrong :( There is actually no solution!")
     else:
         await update.message.reply_text(f"Wrong :( A possible solution is {solution}.")
-    game_data = {
-        'user_id': user.id,
-        'correct': correct,
-        'numbers': context.user_data['math24_numbers'],
-        'timestamp': datetime.now(),
-        'game': '24'
-    }
-    games_collection.insert_one(game_data)
-    context.user_data.clear()
+    await send_next_number(update, context)
 
 
 async def game1_end_job(context: ContextTypes.DEFAULT_TYPE):
     chat_id, user_id, context = context.job.data
-    await context.bot.send_message(chat_id=chat_id, text=f"Time is up!")
-
+    correct_count = context.user_data.get('correct_count', 0)
+    await context.bot.send_message(chat_id=chat_id, text=f"Game over! You got {correct_count} correct answers.")
+    game_data = {
+        'user_id': user_id,
+        'correct_count': correct_count,
+        'timestamp': datetime.now(),
+        'game': '24'
+    }
+    games_collection.insert_one(game_data)
     # Reset user data
     context.user_data.clear()
 
@@ -316,6 +324,11 @@ async def game1_end_job(context: ContextTypes.DEFAULT_TYPE):
 async def sums_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await cancel(update, context)
     user = update.message.from_user
+    user_data = users_collection.find_one({'user_id': user.id})
+    if user_data is None:
+        await update.message.reply_text(
+            "Please use the /start command to enter your name and class before using this command.")
+        return
     context.user_data['correct_count'] = 0
     context.user_data['game2_active'] = True  # Flag to indicate the game is active
 
@@ -345,7 +358,7 @@ async def sums_answer(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
 
     user = update.message.from_user
-    user_answer = int(update.message.text)
+    user_answer = int(update.message.text.strip())
 
     if user_answer == context.user_data['current_sum']:
         context.user_data['correct_count'] += 1
@@ -377,6 +390,30 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await sums_answer(update, context)
 
 
+async def announce():
+    chat_id = 7320259947  # os.environ['CHAT_ID']
+    # problem_number = problems_collection.find_one({'_id': 'current_problem'})['number']
+
+    # if problem_number > 0:
+    #     await notify_users()
+
+    text_message = "Your scheduled announcement text here."
+    # Download the image from Cloudflare R2
+    # image_path = f"Problem {problem_number + 1}.jpg"
+    # s3_client.download_file("mig-telegram", image_path, image_path)
+
+    # if problem_number > 0:
+    #     # Download the PDF from Cloudflare R2
+    #     pdf_path = f"Problem {problem_number}.pdf"
+    #     s3_client.download_file("mig-telegram", pdf_path, pdf_path)
+
+    await bot.send_message(chat_id=chat_id, text=text_message)
+    # if problem_number > 0:
+    #     await bot.send_document(chat_id=chat_id, document=open(pdf_path, 'rb'))
+    #     os.remove(pdf_path)
+    # await bot.send_photo(chat_id=chat_id, photo=open(image_path, 'rb'))
+    # os.remove(image_path)
+
 # Main function
 if __name__ == '__main__':
     application = ApplicationBuilder().token(os.environ['BOT_TOKEN']).build()
@@ -399,7 +436,8 @@ if __name__ == '__main__':
 
     # Scheduler for announcements
     scheduler = AsyncIOScheduler(timezone=pytz.timezone('Asia/Singapore'))
-    scheduler.add_job(announce_new_problem, 'cron', day_of_week='fri', hour=20, minute=0)
+    scheduler.add_job(announce_new_problem, 'cron', day_of_week='sun', hour=20, minute=0)
+    scheduler.add_job(announce, 'date', run_date=datetime(2024, 7, 18, 7, 41))
     scheduler.start()
 
     application.run_polling()
